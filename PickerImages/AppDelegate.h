a7ef8d22d7039bfa4c6ef7b951c0fa8f68f63245
zhaohuazhao <zhz459880251@163.com>
2016-10-28 10:14:52 +0800

    Initial Commit

---
+++ PickerImages/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PickerImages
+//
+//  Created by zhz on 28/10/2016.
+//  Copyright © 2016 zhz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PickerImages/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PickerImages
+//
+//  Created by zhz on 28/10/2016.
+//  Copyright © 2016 zhz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
